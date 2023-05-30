167eeeac0f9957f306ee8774ae1aa3176330093c
Kolya <example@gmail.com>
2023-05-30 13:17:36 +0300

    30.05

---
+++ less3.js
@@ -0,0 +1,98 @@
+if (4 == 5) {
+    console.log('ок');
+} else {
+    console.log('Error')
+}
+
+
+
+const num = 34;
+
+if (num < 49) { 
+    console.log('eror');
+
+} else if (num > 100) {
+    console.log('дохуя');
+} else {
+    console.log('ok');
+}
+
+(num ===50) ? console.log('ok'): console.log('epor');
+
+_________________
+
+const num = 499;
+switch (num) {
+    case 49:
+        console.log('неверно');
+        break;
+        case 100:
+            console.log('неверно');
+            break;
+            case 34:
+                console.log('verno')
+                break;
+                default:
+                    console.log('noyyyyyyy')
+}
+
+
+___________________
+
+const hamburger = true;
+const fries = true;
+
+if (hamburger && fries) {
+    console.log('i am hungrid');
+
+}
+
+const hamburger = 3;
+const fries = 3;
+const kola = 0;
+const nuggets = 2;
+
+
+if (hamburger === 3 && kola === 2  || fries ===3 && nuggets) {
+    console.log('i am hungrid');
+
+} else {
+    console.log('go to home')
+}
+
+_________________
+
+
+
+let qreport, wreport, ereport = 'done'
+
+console.log(qreport || wreport || ereport);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
